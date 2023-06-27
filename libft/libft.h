add48fa1d9e02bf0aa84956f40b7ecb6eb1ad70c
Damien DELHALT <ddelhalt@paul-f4Ar4s2.clusters.42paris.fr>
2023-06-27 13:29:41 +0200

    libft: added ft_strcmp

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hboissel <hboissel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 04:41:08 by hboissel          #+#    #+#             */
-/*   Updated: 2022/11/14 11:52:30 by hboissel         ###   ########.fr       */
+/*   Updated: 2023/06/27 13:26:48 by ddelhalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -62,6 +62,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
